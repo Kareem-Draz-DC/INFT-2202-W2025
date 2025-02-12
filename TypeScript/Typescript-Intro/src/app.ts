a4340aa8a12cb7c2b4fc67fe0554009c8d576592
KareemD123 <kareemdraz.careers@gmail.com>
2025-02-12 18:18:26 -0500

    Typescript intro part 2

--- TypeScript/Typescript-Intro/src/app.ts
+++ TypeScript/Typescript-Intro/src/app.ts
@@ -1,30 +1,52 @@
+import { Movie } from './types/movie.interface.js'
+import { movies } from './data/data.js'
 
+class MovieLibrary {
+    private movies: Movie[];
+    private container: HTMLElement;
 
-// We want to display a list of movies to the user via the DOM
-let movies: string[] = [
-    "Upload - Sci - Fi",
-    "Interstellar",
-    "Brooklyn 99",
-    "The Recruit",
-    "LouderMilk"
-]
+    constructor(movies: Movie[]) {
+        // initialize movie data array
+        this.movies = movies
+        // grab the HTML container
+        const movieList = document.getElementById("movieList")
+        if (!movieList) {
+            throw new Error("Movie List container not found !!!")
+        }
+        this.container = movieList
+    }
 
-function createMoviePost(movie: string) {
-    let container = document.querySelector(".container")
-    let newMoviePost = document.createElement("P")
-    newMoviePost.innerHTML = movie
-    console.log(container)
-    console.log(newMoviePost)
-    container?.appendChild(newMoviePost)
+    private createNewMovieCard(movie: Movie): HTMLDivElement {
+        const card = document.createElement('div')
+        
+        let actorsList = movie.actors?.map(actor => `<li>${actor.firstName} ${actor.lastName}</li>`)
+        
+        card.innerHTML = `
+            <h2>${movie.title}</h2>
+            <div class="movie-info">
+                <p>Director: ${movie.director}</p>
+                <p>Year: ${movie.year}</p>
+                <p>Genre: ${movie.genre}</p>
+                <p>Rating: ${movie.rating}</p>
+                <ul>${actorsList}</ul>
+            </div>
+        `
+        return card
+    }
+
+    public displayMovies(): void {
+        this.movies.forEach(movie => {
+            const movieCard = this.createNewMovieCard(movie)
+            this.container.appendChild(movieCard)
+        })
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     let message: string = "Hello from typescript!"
-    movies.forEach(createMoviePost)
-    // movies.forEach(movie => {
-    //     createMoviePost(movie)
-    // })
-    console.log(message)
+    let library = new MovieLibrary(movies);
+    library.displayMovies();
+    console.log(library)
 })
 
 
